57ab5e8654db65207906afb25f715811c1946872
Jacob Stirling <jacob.stirling.96@outlook.com>
2017-11-19 02:15:45 -0500

    finish PelmorexInterpreter.py

--- PelmorexInterpreter.py
+++ PelmorexInterpreter.py
@@ -4,8 +4,23 @@ from datetime import datetime
 import PelmorexController
 
 
-def nextPrecipToString(location):
+def nextPrecipToString(location, displayRain = True):
     locationCode = getLocationCode(location)
+
+    startRain = -1
+    endRain = -1
+
+    startSnow = -1
+    endSnow = -1
+
+    isCurrentPrecipitation = True
+
+    initiallySnowing = False
+
+    stringType = 0
+
+    outStr = ""
+    addStr = ""
 
     if isinstance(locationCode, int):
         if locationCode == 204:
@@ -19,9 +34,6 @@ def nextPrecipToString(location):
     if isinstance(precipStartStop, int) or isinstance(precipCurrent, int):
         return "An error occurred getting precipitation data: HTTP status code %d." % precipStartStop
 
-    startRain = -1
-    endRain = -1
-
     i = 0
 
     while i < 32:
@@ -30,13 +42,21 @@ def nextPrecipToString(location):
             while i < 32:
                 i += 1
                 if float(precipStartStop["data"][i]["rain"]) == 0:
-                    endRain = i
+                    endRain = i - 1
                     break
             break
         i += 1
 
-    startSnow = -1
-    endSnow = -1
+    if startRain >= 0:
+        startRainDate = datetime.strptime(precipStartStop["data"][startRain]["time"], "%Y-%m-%dT%H:%M:%S")
+        startRainPeriod = int(precipStartStop["data"][startRain]["period"])
+
+    if endRain >= 0:
+        endRainDate = datetime.strptime(precipStartStop["data"][endRain]["time"], "%Y-%m-%dT%H:%M:%S")
+        endRainPeriod = int(precipStartStop["data"][endRain]["period"])
+    else:
+        endRainDate = datetime.strptime(precipStartStop["data"][31]["time"], "%Y-%m-%dT%H:%M:%S")
+        endRainPeriod = int(precipStartStop["data"][31]["period"])
 
     i = 0
 
@@ -46,23 +66,96 @@ def nextPrecipToString(location):
             while i < 32:
                 i += 1
                 if float(precipStartStop["data"][i]["snow"]) == 0:
-                    endSnow = i
+                    endSnow = i - 1
                     break
             break
         i += 1
 
-    isCurrentPrecipitation = True
+    if startSnow >= 0:
+        startSnowDate = datetime.strptime(precipStartStop["data"][startSnow]["time"], "%Y-%m-%dT%H:%M:%S")
+        startSnowPeriod = int(precipStartStop["data"][startSnow]["period"])
+
+    if endSnow >= 0:
+        endSnowDate = datetime.strptime(precipStartStop["data"][endSnow]["time"], "%Y-%m-%dT%H:%M:%S")
+        endSnowPeriod = int(precipStartStop["data"][endSnow]["period"])
+    else:
+        endSnowDate = datetime.strptime(precipStartStop["data"][31]["time"], "%Y-%m-%dT%H:%M:%S")
+        endSnowPeriod = int(precipStartStop["data"][31]["period"])
 
     eventsArray = precipCurrent["data"]["events"]
     eventsArrayStr = json.dumps(eventsArray)
     if "[]" in eventsArrayStr:
         isCurrentPrecipitation = False
 
-    print(startRain)
-    print(endRain)
-    print(startSnow)
-    print(endSnow)
-    print(isCurrentPrecipitation)
+    if isCurrentPrecipitation:
+        startPrecip = int(precipCurrent["data"]["events"][0]["startType"])
+
+        if startPrecip == 3 or startPrecip == 4:
+            initiallySnowing = True
+
+    if displayRain:
+        if isCurrentPrecipitation and not initiallySnowing:
+            stringType = 1
+            outStr = "It is currently raining. The rain will "
+        elif startRain != -1:
+            stringType = 2
+            outStr = "It is going to rain on the "
+        else:
+            stringType = 3
+            outStr = "There will be no rain over the next week."
+    else:
+        if isCurrentPrecipitation and initiallySnowing:
+            stringType = 4
+            outStr = "It is currently snowing. The snow will "
+        elif startSnow != -1:
+            stringType = 5
+            outStr = "It is going to snow on the "
+        else:
+            stringType = 6
+            outStr = "There will be no snow over the next week."
+
+    if stringType == 1:
+        if endRain >= 0:
+            addStr = "continue until the " + getTimeOfDay(endRainPeriod) + " of " + endRainDate.strftime("%Y-%m-%d") + \
+                "."
+            outStr = outStr + addStr
+        else:
+            addStr = "continue past the " + getTimeOfDay(endRainPeriod) + " of " + endRainDate.strftime("%Y-%m-%d") + \
+                "."
+            outStr = outStr + addStr
+    elif stringType == 2:
+        if endRain >= 0:
+            addStr = getTimeOfDay(startRainPeriod) + " of " + startRainDate.strftime("%Y-%m-%d") + \
+                " and will continue until the " + getTimeOfDay(endRainPeriod) + " of " + \
+                endRainDate.strftime("%Y-%m-%d") + "."
+            outStr = outStr + addStr
+        else:
+            addStr = getTimeOfDay(startRainPeriod) + " of " + startRainDate.strftime("%Y-%m-%d") + \
+                " and will continue past the " + getTimeOfDay(endRainPeriod) + " of " + \
+                endRainDate.strftime("%Y-%m-%d") + "."
+            outStr = outStr + addStr
+    elif stringType == 4:
+        if endSnow >= 0:
+            addStr = "continue until the " + getTimeOfDay(endSnowPeriod) + " of " + endSnowDate.strftime("%Y-%m-%d") + \
+                "."
+            outStr = outStr + addStr
+        else:
+            addStr = "continue past the " + getTimeOfDay(endSnowPeriod) + " of " + endSnowDate.strftime("%Y-%m-%d") + \
+                "."
+            outStr = outStr + addStr
+    elif stringType == 5:
+        if endSnow >= 0:
+            addStr = getTimeOfDay(startSnowPeriod) + " of " + startSnowDate.strftime("%Y-%m-%d") + \
+                " and will continue until the " + getTimeOfDay(endSnowPeriod) + " of " + \
+                endSnowDate.strftime("%Y-%m-%d") + "."
+            outStr = outStr + addStr
+        else:
+            addStr = getTimeOfDay(startSnowPeriod) + " of " + startSnowDate.strftime("%Y-%m-%d") + \
+                " and will continue past the " + getTimeOfDay(endSnowPeriod) + " of " + \
+                endSnowDate.strftime("%Y-%m-%d") + "."
+            outStr = outStr + addStr
+
+    return outStr
 
 
 def getLocationCode(location):
@@ -74,4 +167,12 @@ def getLocationCode(location):
     return locationData["dataCode"]
 
 
-nextPrecipToString("london%20on")
+def getTimeOfDay(period):
+    if period == 1:
+        return "evening"
+    elif period == 2:
+        return "night"
+    elif period == 3:
+        return "morning"
+    else:
+        return "afternoon"
